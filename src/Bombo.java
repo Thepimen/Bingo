62a6f9f3d5a835dce87d3bcef26ca3a3ba50be01
Thepimen <luislazaropimentel@gmail.com>
2026-01-29 11:01:12 +0100

    Añadir archivos actualizados

--- src/Bombo.java
+++ src/Bombo.java
@@ -1,5 +1,25 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Bombo {
-    
+    private ArrayList<Integer> bolas; //Generar un arraylist que equivaldría a las bolas 
+                                      // dentro del bombo
+    public Bombo(){
+        bolas = new ArrayList<>();
+        for(int i = 0; i < 90; i++){ //Rellenar el bombo con las bolas del 1 al 90 incluidos
+            bolas.add(i);
+        }
+        Collections.shuffle(bolas); //Se barajan las bolas para que salgan aleatorias
+    } 
+
+    public int sacarbola(){
+        if(bolas.isEmpty()){
+            return -1;
+        }
+        return bolas.remove(0);
+    }
+
+    public boolean quedanBolas(){
+        return !bolas.isEmpty();
+    }
 }
--- src/Carton.java
+++ src/Carton.java
@@ -1,3 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 public class Carton {
+    private Map<Integer, Boolean> numeros;
+
+    private void generarCarton(){
+        Random rand = new Random();
+        while(numeros.size() < 15){
+            int num = rand.nextInt(90) + 1;
+            numeros.putIfAbsent(num, false); 
+            //Este put solo va a añadir si NO EXISTE la clave, se inicializa a false
+            //Para que sea no tachado
+        }
+    }
+
+    public void comprobarNumero(int bola){
+        if(numeros.containsKey(bola)){
+            numeros.put(bola, true);
+            System.out.println("Numero tachado: "+ bola + "!");
+        }
+    }
+
+    public boolean esBingo(){
+        return !numeros.containsValue(false);
+    }
+
+    public void mostrarCarton(){
+        System.out.println("Carton: "+ numeros.keySet());
+    }
+
+    public Carton(){
+        numeros = new HashMap<>();
+        generarCarton();
+    }
     
 }
